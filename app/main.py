b78bb949f1397e133f93cd2257dd0d5cdf5de73c
Dominik <690828@student.inholland.nl>
2024-12-18 15:37:50 +0100

    Test linting

--- app/main.py
+++ app/main.py
@@ -331,3 +331,5 @@ async def increment_clicks(category: str, faq_id: str):
     except Exception:
         logger.error(failedToFetchFaqErrorMsg, exc_info=True)
         raise HTTPException(status_code=500, detail=internalServerErrorMsg)
+
+# Test
