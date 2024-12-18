b99a126203be0971a92a4835f32140fcb0aa7cc7
Vincent Evers <44191940+EarlyBirdREAL@users.noreply.github.com>
2024-12-18 10:04:30 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import logging
 import uuid
 
 # Initialize FastAPI app
-# added a comment to check the workings of workflows
+# added a comment to check the workings of workflows v2.0
 app = FastAPI()
 
 logger = logging.getLogger(__name__)
